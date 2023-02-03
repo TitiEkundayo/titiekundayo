3ffcd1674c6dc511c8246d81361b86d607ce7c20
profgreatwonder <muoghaluchidinma@gmail.com>
2023-02-03 18:19:16 +0100

    new branch and contact page added

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,7 @@
 import { ContactNavTitle } from "@/components/Navbar";
 import UnderConstruction1 from "./underconstruction1";
 import Footer from "@/components/Footer";
+import { EmailIcon, SocialMediaIcons } from "@/components/icons.js/SocialMediaIcons";
 
 const Contact = () => {
 	return (
@@ -15,5 +16,41 @@ const Contact = () => {
 		</div>
 	);
 };
+// const Contact = () => {
+// 	return (
+// 		<div>
+// 			<div>
+// 				<ContactNavTitle />
+// 			</div>
+
+// 			<div>
+// 				<UnderConstruction1 />
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+
+const GetInTouch = ()=> {
+	return (
+		<>
+			<div>
+				<h2>Get In Touch</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum?</p>
+			</div>
+
+			{/* <div>
+				<EmailIcon />
+				<p>titiekundayo@gmail.com</p>
+			</div> */}
+
+			<div>
+				<SocialMediaIcons/>
+			</div>
+
+
+		</>
+	)
+}
 
 export default Contact;
