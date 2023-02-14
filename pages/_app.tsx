28521adc17bf1a90ae1fd8bb19acc812ab993722
Inioluwa <inioluwadeborah2@gmail.com>
2023-02-14 20:17:27 +0100

    updatingthebookpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
 	useEffect(() => {
