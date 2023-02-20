35e78296826922bc2ee6166a69c06ddc51bcae00
opeyemibioku <opeyemibioku@gmail.com>
2023-02-20 23:37:53 +0100

    worked on the consulting page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "../styles/componentstyles/landingpage.module.scss";
+import consultingstyles from "../styles/componentstyles/consulting.module.scss";
 import titi from "../public/images/HomeImgs/titi.png";
 // import styles from "../styles/generalstyles/colors.module.scss";
 
@@ -7,6 +8,41 @@ const Hero = () => {
   return <div>Hero</div>;
 };
 
+export const ConsultingPageHero = () => {
+  return (
+    <>
+      <div
+        className={`container-fluid d-flex align-items-center ${consultingstyles.ConsultingPageHeroBg}`}
+      >
+        <div
+          className={`px-5 container-fluid d-flex ${consultingstyles.ConsultingPageHeroBgg}`}
+        >
+          <div className={`row align-items-center py-3`}>
+            <div className={`col-lg-5 col-md-7 col-sm-12 pt-3`}>
+              <h2>
+                Lorem Ipsum Dolor
+                <br />
+              </h2>
+              <p className={`mt-1`}>
+                Lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem
+                ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum
+                dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor
+              </p>
+              <div className={`text-start mb-4`}></div>
+            </div>
+            {/* <div className={`col-lg-5 col-md-5 col-sm-12`}>
+              <Image
+                src={titi}
+                className="d-block mx-lg-auto img-fluid"
+                alt=""
+              />
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export const LandingPageHero = () => {
   return (
     <>
@@ -36,7 +72,6 @@ export const LandingPageHero = () => {
                 src={titi}
                 className="d-block mx-lg-auto img-fluid"
                 alt=""
-                // width="500rem"
               />
             </div>
           </div>
